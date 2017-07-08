0f0a23ec9f81bab10e0dbe12c80a9112759b1355
btotals <qianyedaodaozhai@163.com>
2017-07-08 23:03:15 +0800

    as

--- config.js
+++ config.js
@@ -5,12 +5,6 @@ module.exports = {
         'port': '80',
     },
     'appConfig': {
-        'appId': 'wxe5f52902cf4de896',
-        'secret': '5e8b72a07287895d83a098f7af270fb1',
-        'GMD_appId': 'wx5946b07343b1ee6e',
-        'GMD_secret': '4c635b77b82d988e0908a893867f6def',
-        'mchId': '1900006631',
-        'key': 'f8bfc0afd58b1bc156ee34e1241e0043',
-        'templateId': 'fvwQDMMRTuS8N4cgA05WlPx0y3el74SmwRIwKmpEA2I'
+
     }
 };
