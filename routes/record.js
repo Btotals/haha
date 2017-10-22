00d8ea71ef326679853e622e414a378725d72685
totalxiao(肖韬韬) <totalxiao@tencent.com>
2017-10-22 21:20:03 +0800

    up

--- routes/record.js
+++ routes/record.js
@@ -34,7 +34,7 @@ module.exports = {
 function postRecord(req, res) {
   var params = req.body
 
-  console.log('post record with params', JSON.stringify(params))
+  // console.log('post record with params', JSON.stringify(params))
 
   // var questionId = params.questionId
   // var openId = params.openId
@@ -58,38 +58,47 @@ function downloadRecord(req, res) {
 }
 
 function getUserState(req, res) {
-  var openid = req.query.openid
-  console.log('openid', openid)
-  fs.readFile('../data.csv', 'utf-8', function(err, data) {
-    console.log('err is', err)
-    parse(data, function(error, output) {
-      console.log('err is', error)
-      if (error) {
-        res.end(JSON.stringify(error))
-        return
-      }
-      console.log('output is', output)
-      var userState = output.find(function(item) {
-        return item[0] === openid
-      })
+  // var openid = req.query.openid
+  // var questionId = req.query.questionId
+  // console.log('openid', openid, 'questionId', questionId)
 
-      var userInfo
-
-      if (userState) {
-        userInfo = {
-          existed: true,
-          userState
-        }
-      } else {
-        userInfo = {
-          existed: false,
-          userState: null
-        }
-      }
-
-      res.end(JSON.stringify(userInfo))
-    })
+  crud.getGrade(req.query, function(err, data) {
+    if (err) {
+      res.end(JSON.stringify(err))
+    } else {
+      res.end(JSON.stringify(data))
+    }
   })
+  // fs.readFile('../data.csv', 'utf-8', function(err, data) {
+  //   console.log('err is', err)
+  //   parse(data, function(error, output) {
+  //     console.log('err is', error)
+  //     if (error) {
+  //       res.end(JSON.stringify(error))
+  //       return
+  //     }
+  //     console.log('output is', output)
+  //     var userState = output.find(function(item) {
+  //       return item[0] === openid
+  //     })
+
+  //     var userInfo
+
+  //     if (userState) {
+  //       userInfo = {
+  //         existed: true,
+  //         userState
+  //       }
+  //     } else {
+  //       userInfo = {
+  //         existed: false,
+  //         userState: null
+  //       }
+  //     }
+
+  //     res.end(JSON.stringify(userInfo))
+  //   })
+  // })
 }
 
 function saveOpinion(req, res) {
